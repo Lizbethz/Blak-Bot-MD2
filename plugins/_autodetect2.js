7f1c6b00d4bb3295343929ac1afd95a83b81c5cb
Mariela <sosalopezmariela@gmail.com>
2025-01-26 13:11:52 -0600

    Update _autodetect2.js

--- plugins/_autodetect2.js
+++ plugins/_autodetect2.js
@@ -8,8 +8,8 @@ let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'  
 
 let bienvenida
-//agregado = `*🍬 Ha llegado un nuevo participante al grupo.*\n\n*◦ 🍫 Grupo:* ${groupMetadata.subject}\n\n*◦ 🍭 Bienvenido/a:* @${m.messageStubParameters[0].split('@')[0]} ingresado al grupo\n\n*◦ 🍭 Añadido por:* @${m.sender.split('@')[0]} que añadió a @${m.messageStubParameters[0].split('@')[0]} al grupo`
-bienvenida = `*🍬 Ha llegado un nuevo participante al grupo.*\n\n*◦ 🍫 Grupo:* ${groupMetadata.subject}\n\n*◦ 🍭 Bienvenido/a:* @${m.messageStubParameters[0].split('@')[0]} ingresado al grupo\n\n*◦ 🍭 Aceptado por:* @${m.sender.split('@')[0]} que aceptó la solicitud de @${m.messageStubParameters[0].split('@')[0]} a ingresar al grupo`    
+//agregado = `*🤖 Ha llegado un nuevo participante al grupo.*\n\n*◦ 💓 Grupo:* ${groupMetadata.subject}\n\n*◦ 🥳 Bienvenido/a:* @${m.messageStubParameters[0].split('@')[0]} ingresado al grupo\n\n*◦ ❤️ Añadido por:* @${m.sender.split('@')[0]} que añadió a @${m.messageStubParameters[0].split('@')[0]} al grupo`
+bienvenida = `*🤖 Ha llegado un nuevo participante al grupo.*\n\n*◦ 💓 Grupo:* ${groupMetadata.subject}\n\n*◦ 🥳 Bienvenido/a:* @${m.messageStubParameters[0].split('@')[0]} ingresado al grupo\n\n*◦ ❤️ Aceptado por:* @${m.sender.split('@')[0]} que aceptó la solicitud de @${m.messageStubParameters[0].split('@')[0]} a ingresar al grupo`    
 
 //if (chat.detect2 && m.messageStubType == 27) {
 //await conn.sendMessage(m.chat, { text: agregado, mentions: [`${m.messageStubParameters[0]}`, `${m.sender}`] }, { quoted: fkontak })
